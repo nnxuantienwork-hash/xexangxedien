bd511f86d62836fcd814e723509334c632ad1fb8
rxn <nnxuantienwork@gmail.com>
2026-05-01 12:22:15 +0700

    ud

--- app/page.tsx
+++ app/page.tsx
@@ -176,7 +176,7 @@ export default function LongformArticle() {
 </section>
 
 {/* Split Image 2 - After anh Nhã quote about 10-12 hours */}
-<section className="w-full bg-white py-3">
+<section className="w-full bg-white py-12">
   <SplitImage 
     leftImage="/images/xe-may-1.png"
     rightImage="/images/tram-sac-2.jpg"
